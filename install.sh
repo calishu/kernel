60773c147fd1d211f9b6fe881aea7658eea3bc6e
calishu <danny.kretschmann@proton.me>
2026-01-31 00:00:01 +0100

    feat: add install script for limine

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir .temp
+cd .temp
+git clone https://codeberg.org/Limine/Limine.git --branch=v10.x-binary --depth=1
+cd ./Limine/
+make
+
+mkdir -p iso_root/EFI/BOOT/
+
+mv ./BOOTX64.EFI ../../iso_root/EFI/BOOT/
+mv ./limine ../../
+mv ./limine-bios.sys ../../iso_root/
+mv ./limine-bios-cd.bin ../../iso_root/
+mv ./limine-uefi-cd.bin ../../iso_root
+
+cd ../../
+rm -rf .temp/
